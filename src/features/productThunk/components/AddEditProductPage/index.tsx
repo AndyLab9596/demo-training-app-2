73be00e1cc52e8ab22d05e86a554c74f74190369
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-11-12 07:38:37 +0700

    building addEditProductPage

--- src/features/productThunk/components/AddEditProductPage/index.tsx
+++ src/features/productThunk/components/AddEditProductPage/index.tsx
@@ -1,9 +1,38 @@
-import React from "react";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import productApi from "../../../../api/productApi";
+import { Product } from "../../../../models";
+import "./AddEditProductListPage.scss";
 
 const AddEditProductListPage = () => {
+  const params = useParams<{ productId: string }>();
+  const productId = params?.productId;
+  const isEdit = !!productId;
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    if (!isEdit) return;
+
+    (async () => {
+      try {
+        const response = await productApi.getProductById(productId);
+        setProduct(response);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [productId, isEdit]);
+
   return (
-    <div>
-      <h3>Add Edit Product List Page</h3>
+    <div className="container">
+      {/* Navigation */}
+      <Link to="/adminThunk/product">
+        <ArrowLeftOutlined /> Back To Product List
+      </Link>
+
+      <div></div>
     </div>
   );
 };
